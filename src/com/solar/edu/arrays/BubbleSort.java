54a96a281a60105afbbf11471865b451646bf414
Ajay Singh <ajayhasanemail@gmail.com>
2021-12-25 13:40:29 -0500

    Add method to bubble sort integer arrays in ascending order

---
+++ src/com/solar/edu/arrays/BubbleSort.java
@@ -0,0 +1,23 @@
+package com.solar.edu.arrays;
+
+import com.solar.edu.shared.Utils;
+
+public class BubbleSort {
+     public static void sortAsc (int[] array) {
+        if ((array == null) || (array.length == 0)) return;
+        if (array.length == 1) {
+            Utils.printArray(array);
+            return;
+        }
+
+        int unsortedIndex = array.length - 1;
+        while (unsortedIndex-- > 0) {
+            for (int i = 0; i <= unsortedIndex; i++) {
+                if (i == array.length - 1) continue;
+                if (array[i] > array[i+1]) Utils.swap(array, i, i + 1);
+            }
+        }
+
+        Utils.printArray(array);
+    }
+}
