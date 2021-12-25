9f9c4c182c5170bbe4ec599359d46988845eb84c
Ajay Singh <ajayhasanemail@gmail.com>
2021-12-25 13:39:55 -0500

    Add method to swap elements in an integer array

--- src/com/solar/edu/shared/Utils.java
+++ src/com/solar/edu/shared/Utils.java
@@ -8,4 +8,13 @@ public class Utils {
         for (int el: array) System.out.print(el + ", ");
         System.out.print("]");
     }
+
+    public static void swap(int[] array, int firstIndex, int secondIndex) {
+        if (array == null || array.length < 2 || firstIndex > array.length - 1 || secondIndex > array.length - 1) return;
+        if (firstIndex < 0 || secondIndex < 0 || firstIndex == secondIndex || array[firstIndex] == array[secondIndex]) return;
+
+        final int temp = array[firstIndex];
+        array[firstIndex] = array[secondIndex];
+        array[secondIndex] = temp;
+    }
 }
