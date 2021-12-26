431c89d4fa6eefa2232e4b6629bad7082a889a87
Ajay Singh <ajayhasanemail@gmail.com>
2021-12-25 20:43:30 -0500

    Add selection sort class and method to sort ascending

---
+++ src/com/solar/edu/arrays/SelectionSort.java
@@ -0,0 +1,27 @@
+package com.solar.edu.arrays;
+
+import com.solar.edu.shared.Utils;
+
+public class SelectionSort {
+    public static void sortAsc (int[] array) {
+        if (array == null || array.length == 0) return;
+        if (array.length == 1) {
+            Utils.printArray(array);
+            return;
+        }
+
+        for (int sortedIndex = array.length - 1; sortedIndex > 0; sortedIndex--) {
+            int currentMax = Integer.MIN_VALUE;
+            int indexOfCurrentMax = -1;
+            for (int unsortedIndex = 0; unsortedIndex <= sortedIndex; unsortedIndex++)
+                if (array[unsortedIndex] > currentMax) {
+                    currentMax = array[unsortedIndex];
+                    indexOfCurrentMax = unsortedIndex;
+                }
+
+            Utils.swap(array, indexOfCurrentMax, sortedIndex);
+        }
+
+        Utils.printArray(array);
+    }
+}
