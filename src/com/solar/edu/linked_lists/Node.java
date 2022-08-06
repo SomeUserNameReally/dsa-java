1a722aefa0d2fb89571848c733eb56305baf571c
Ajay Singh <ajayhasanemail@gmail.com>
2022-08-06 19:47:16 -0400

    Override method to allow more sensible string representation

--- src/com/solar/edu/linked_lists/Node.java
+++ src/com/solar/edu/linked_lists/Node.java
@@ -23,4 +23,9 @@ public class Node<T> {
     public void setNext(Node<T> next) {
         this.next = next;
     }
+
+    @Override
+    public String toString() {
+        return this.getO().toString();
+    }
 }
