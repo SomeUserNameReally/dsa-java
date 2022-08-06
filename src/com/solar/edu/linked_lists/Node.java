2db9150de02368f0205ae63dc4399e465f5960b7
Ajay Singh <ajayhasanemail@gmail.com>
2022-08-06 17:11:17 -0400

    Define node class for singly linked list

---
+++ src/com/solar/edu/linked_lists/Node.java
@@ -0,0 +1,26 @@
+package com.solar.edu.linked_lists;
+
+public class Node<T> {
+    private T o;
+    private Node<T> next;
+
+    public Node(T o) {
+        this.o = o;
+    }
+
+    public T getO() {
+        return o;
+    }
+
+    public Node<T> getNext() {
+        return next;
+    }
+
+    public void setO(T o) {
+        this.o = o;
+    }
+
+    public void setNext(Node<T> next) {
+        this.next = next;
+    }
+}
