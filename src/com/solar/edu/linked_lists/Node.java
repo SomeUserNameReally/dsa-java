9cb1b09e6581c5f6d56e9c8a3b2ad4ac7fabdd95
Ajay Singh <ajayhasanemail@gmail.com>
2022-08-08 16:55:36 -0400

    Adjust node to allow reference to previous node, use consistent code style

--- src/com/solar/edu/linked_lists/Node.java
+++ src/com/solar/edu/linked_lists/Node.java
@@ -3,21 +3,30 @@ package com.solar.edu.linked_lists;
 public class Node<T> {
     private final T o;
     private Node<T> next;
+    private Node<T> previous;
 
     public Node(T o) {
         this.o = o;
     }
 
     public T getO() {
-        return o;
+        return this.o;
     }
 
     public Node<T> getNext() {
-        return next;
+        return this.next;
     }
 
     public void setNext(Node<T> next) {
         this.next = next;
+    }
+
+    public Node<T> getPrevious() {
+        return this.previous;
+    }
+
+    public void setPrevious(Node<T> previous) {
+        this.previous = previous;
     }
 
     @Override
