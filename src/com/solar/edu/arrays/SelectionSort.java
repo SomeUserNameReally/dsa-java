325300a49b24db0a3be2634db11c5c4a93b7e5a7
Ajay Singh <ajayhasanemail@gmail.com>
2022-08-15 05:30:42 -0400

    Streamline logic for sorting arrays

--- src/com/solar/edu/arrays/SelectionSort.java
+++ src/com/solar/edu/arrays/SelectionSort.java
@@ -3,20 +3,18 @@ package com.solar.edu.arrays;
 import com.solar.edu.shared.Utils;
 
 public class SelectionSort {
-    public static void sortAsc (int[] array) {
+    public static void sortAsc(int[] array) {
         if (array == null || array.length == 0) return;
-        if (array.length == 1) {
-            Utils.printArray(array);
-            return;
-        }
+        
+        if (array.length > 1) {
+            for (int sortedIndex = array.length - 1; sortedIndex > 0; sortedIndex--) {
+                int indexOfCurrentMax = 0;
+                for (int unsortedIndex = 0; unsortedIndex <= sortedIndex; unsortedIndex++)
+                    if (array[unsortedIndex] > array[indexOfCurrentMax])
+                        indexOfCurrentMax = unsortedIndex;
 
-        for (int sortedIndex = array.length - 1; sortedIndex > 0; sortedIndex--) {
-            int indexOfCurrentMax = 0;
-            for (int unsortedIndex = 0; unsortedIndex <= sortedIndex; unsortedIndex++)
-                if (array[unsortedIndex] > array[indexOfCurrentMax])
-                    indexOfCurrentMax = unsortedIndex;
-
-            Utils.swap(array, indexOfCurrentMax, sortedIndex);
+                Utils.swap(array, indexOfCurrentMax, sortedIndex);
+            }
         }
 
         Utils.printArray(array);
